36ebab406c0bf1d49a54cffbb1f97afd7be3ab6e
Nmuta Jones <nmuta1@gmail.com>
2017-09-11 14:06:25 -0700

    adding new tests

--- CrackCodingInt/src/cracking_coding_interview/IntervewQuestions.java
+++ CrackCodingInt/src/cracking_coding_interview/IntervewQuestions.java
@@ -5,40 +5,53 @@ public class IntervewQuestions {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
-	
+
 	public static String splitString(String str, String delimiter, int interval){
-		String result = ""; 
+		String result = "";
 		// should return the string provided, split with the given delimiter at given interval.
-		// There should be no trailing delimiters at the end of the word. 
-		// e.g. splitString("banana", "-", 2) should return "ba-na-na" 
-		return result; 
+		// There should be no trailing delimiters at the end of the word.
+		// e.g. splitString("banana", "-", 2) should return "ba-na-na"
+		return result;
 	}
-	
+
 	public static boolean fuzzyMatch(String string1, String string2){
 		boolean result = false;
-		// Tests if two string are mostly identical with one "bad" character allowance. 
-		// e.g.   fuzzyMatch("banana", "banona")  should return true. 
+		// Tests if two string are mostly identical with one "bad" character allowance.
+		// e.g.   fuzzyMatch("banana", "banona")  should return true.
 
-		return result; 
+		return result;
 	}
-	
+
 	public static String reverseMe(String str){
-		String result = ""; 
-		// should return a reversed string. 	 
-		return result; 
+		String result = "";
+		// should return a reversed string.
+		return result;
 	}
-	
+
 	public boolean isPrime(int incoming){
-		return false; 
+		return false;
 	}
-	
+
+  // returns the last word in an string of words.
+	// lastWord("the blue sky plane") should return 'plane'
+	public String lastWord(String incoming){
+		return "";
+	}
+
+  // return the tip amount only.  tipCalc(10.00) should return 2.0
+	// don't worry about currency format ... we'll cover that after
+	// we talk about using packages  
+	public double tipCalc(double incoming, double tip_percentage){
+		return 0.0;
+	}
+
 	public String getPenultimateNode(Node node_in){
-		// accepts the FIRST NODE OF A SINGLY LINKED LIST, and then returns the 
-		// value of the penultimate Node in that list. 
-		// PROVIDE STUDENTS WITH NODE CLASS on whiteboard before beginning. (Node.java) 
-		
-		//return node_in.data; 
-		return ""; 
+		// accepts the FIRST NODE OF A SINGLY LINKED LIST, and then returns the
+		// value of the penultimate Node in that list.
+		// PROVIDE STUDENTS WITH NODE CLASS on whiteboard before beginning. (Node.java)
+
+		//return node_in.data;
+		return "";
 	}
 
 }
--- CrackCodingInt/src/cracking_coding_interview/IntervewQuestionsTest.java
+++ CrackCodingInt/src/cracking_coding_interview/IntervewQuestionsTest.java
@@ -10,17 +10,17 @@ public class IntervewQuestionsTest extends IntervewQuestions {
 	public void split_string_test() {
 		assertEquals(splitString("banana", "-", 2), "ba-na-na" );
 	}
-	
+
 	@Test
 	public void fuzzy_match_test() {
 		assertTrue(fuzzyMatch("banana", "banona"));
 	}
-	
+
 	@Test
 	public void reverseMe_test() {
 		assertEquals(reverseMe("cat"),  "tac");
 	}
-	
+
 	@Test
 	public void isPrime_test() {
 		assertEquals(isPrime(2),  true);
@@ -28,17 +28,29 @@ public class IntervewQuestionsTest extends IntervewQuestions {
 		assertEquals(isPrime(997),  true);
 		assertEquals(isPrime(23),  true);
 	}
-	
+
+	@Test
+	public void lastWord_test() {
+		String sentence = "The quick brown fox";
+		assertEquals(lastWord(sentence),  "fox");
+	}
+
+	@Test
+	public void tipCalc_test() {
+		double bill = 10.00;
+		assertEquals(tipCalc(bill,0.2),  2.0);
+	}
+
 	@Test
 	public void getPenultimateNode_test() {
 		Node colorado = new Node("colorado");
 		Node utah = new Node("utah");
 		Node nevada = new Node("nevada");
 		Node california = new Node("california");
-		
+
 		assertEquals(getPenultimateNode(colorado),  "nevada");
 	}
-	
+
 
 
 }
